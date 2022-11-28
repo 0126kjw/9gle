45b26466a99e9c30a4f1ddb4054919556d80be39
Jinwoo Kim <0126kjw@kakao.com>
2022-11-28 19:59:42 +0900

    Update ApiTags, ApiOperation

--- src/search/search.controller.ts
+++ src/search/search.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { search } from './dto/search.dto';
 import { SearchService } from './search.service';
 
+@ApiTags('Search')
 @Controller('search')
 export class SearchController {
   constructor(private searchService: SearchService) {}
 
+  @ApiOperation({ summary: '검색 기능' })
   @Get()
   async search(@Query() searchDto: search): Promise<object> {
     const searchData = await this.searchService.search(
