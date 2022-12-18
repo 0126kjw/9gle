a2b5d4dd4c1ce0c50da5fc2da85a439fbf7b2303
Jinwoo Kim <0126kjw@kakao.com>
2022-12-18 22:08:53 +0900

    Fix import code

--- src/modules/map/schemas/map.schema.ts
+++ src/modules/map/schemas/map.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import mongoose, { Document } from 'mongoose';
+import { Document } from 'mongoose';
 
 export type MapDocument = Map & Document;
 
